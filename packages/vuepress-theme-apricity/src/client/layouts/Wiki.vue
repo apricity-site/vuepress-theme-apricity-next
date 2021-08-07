6cfc423fa76a30e9567d097618f3649816b5d1c1
liusong <treeshade_liu@163.com>
2021-08-07 11:00:10 +0800

    feat(theme-apricity): 初步确认wiki方向

--- packages/vuepress-theme-apricity/src/client/layouts/Wiki.vue
+++ packages/vuepress-theme-apricity/src/client/layouts/Wiki.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>wiki</div>
+  <div class="wiki-container">
+    <nav>
+      <div class="logo">
+        <span>W</span>
+        <span>Personal Wiki</span>
+      </div>
+    </nav>
+    <section>project</section>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
