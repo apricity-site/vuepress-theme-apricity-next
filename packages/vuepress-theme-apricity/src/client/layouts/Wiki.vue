e0904c0560b4723dcc58db8d29056a9107e7d590
liusong <treeshade_liu@163.com>
2021-08-08 16:27:46 +0800

    feat(theme-apricity): 使支持naive UI

--- packages/vuepress-theme-apricity/src/client/layouts/Wiki.vue
+++ packages/vuepress-theme-apricity/src/client/layouts/Wiki.vue
@@ -4,8 +4,11 @@
       <nav class="flex flex-col items-center w-48">
         <span>W</span>
         <span>Personal Wiki</span>
+        <NMenu :options="menuOptions" />
       </nav>
-      <section class="flex-grow">project</section>
+      <section class="flex-grow">
+        <NButton>button</NButton>
+      </section>
     </div>
   </div>
 </template>
@@ -13,18 +16,93 @@
 import { defineComponent } from 'vue'
 import { withBase } from '@vuepress/client'
 import { useThemeData } from '../composables'
+import { NButton, NMenu } from 'naive-ui'
+
+const menuOptions = [
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    href: 'https://baike.baidu.com/item/%E4%B8%94%E5%90%AC%E9%A3%8E%E5%90%9F/3199',
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    disabled: true,
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+      },
+    ],
+  },
+  {
+    label: '寻羊冒险记',
+    key: 'a-wild-sheep-chase',
+    disabled: true,
+  },
+  {
+    label: '舞，舞，舞',
+    key: 'dance-dance-dance',
+    children: [
+      {
+        type: 'group',
+        label: '人物',
+        key: 'people',
+        children: [
+          {
+            label: '叙事者',
+            key: 'narrator',
+          },
+          {
+            label: '羊男',
+            key: 'sheep-man',
+          },
+        ],
+      },
+      {
+        label: '饮品',
+        key: 'beverage',
+        children: [
+          {
+            label: '威士忌',
+            key: 'whisky',
+            href: 'https://baike.baidu.com/item/%E5%A8%81%E5%A3%AB%E5%BF%8C%E9%85%92/2959816?fromtitle=%E5%A8%81%E5%A3%AB%E5%BF%8C&fromid=573&fr=aladdin',
+          },
+        ],
+      },
+      {
+        label: '食物',
+        key: 'food',
+        children: [
+          {
+            label: '三明治',
+            key: 'sandwich',
+          },
+        ],
+      },
+      {
+        label: '过去增多，未来减少',
+        key: 'the-past-increases-the-future-recedes',
+      },
+    ],
+  },
+]
 export default defineComponent({
   name: 'Wiki',
+  components: {
+    NButton,
+    NMenu,
+  },
   setup() {
     const wrapperStyle = {
       backgroundImage: `url(${withBase('/wiki/index-1.jpg')})`,
     }
     const themeData = useThemeData()
     const workspaces = [...themeData.value.wiki]
-    console.log(workspaces)
     return {
       wrapperStyle,
       workspaces,
+      menuOptions,
     }
   },
 })
